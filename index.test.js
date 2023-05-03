75de443fcf28ca5fd3a1c7cba5717df5345013c2
Meher Tasnim <mehertasnim95@gmail.com>
2023-05-03 16:18:26 -0500

    added 3 test cases of band

--- index.test.js
+++ index.test.js
@@ -1,4 +1,4 @@
-const { sequelize } = require('./db');
+const { sequelize, db } = require('./db');
 const { Band, Musician, Song } = require('./index')
 
 describe('Band, Musician, and Song Models', () => {
@@ -9,13 +9,17 @@ describe('Band, Musician, and Song Models', () => {
         // the 'sync' method will create tables based on the model class
         // by setting 'force:true' the tables are recreated each time the 
         // test suite is run
-        await sequelize.sync({ force: true });
+        await db.sync({ force: true });
     })
 
     test('can create a Band', async () => {
         // TODO - test creating a band
-        expect('NO TEST').toBe('EXPECTED VALUE HERE');
-    })
+        const testBand = await Band.create({
+            name: 'Backstreet boys',
+            genre: 'Pop'
+        });
+        expect(testBand.name).toBe('Backstreet boys');
+    });
 
     test('can create a Musician', async () => {
         // TODO - test creating a musician
@@ -24,8 +28,12 @@ describe('Band, Musician, and Song Models', () => {
 
     test('can update a Band', async () => {
         // TODO - test updating a band
-        expect('NO TEST').toBe('EXPECTED VALUE HERE');
-    })
+        let updatedBand = await Band.findByPk(1);
+        const newBand = await updatedBand.update({
+            name: 'Spice Girls'
+        });
+        expect(newBand.name).toBe('Spice Girls');
+    });
 
     test('can update a Musician', async () => {
         // TODO - test updating a musician
@@ -34,8 +42,11 @@ describe('Band, Musician, and Song Models', () => {
 
     test('can delete a Band', async () => {
         // TODO - test deleting a band
-        expect('NO TEST').toBe('EXPECTED VALUE HERE');
-    })
+        let findBand = await Band.findByPk(1);
+        let deleteBand = await findBand.destroy();
+        let emptyBand = await Band.findByPk(1);
+        expect(emptyBand).toBe(null);
+    });
 
     test('can delete a Musician', async () => {
         // TODO - test deleting a musician
