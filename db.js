df610ad437982686db0a28eb124fa25cc46cf88b
Meher Tasnim <mehertasnim95@gmail.com>
2023-05-03 14:49:17 -0500

    added db.sqlite

--- db.js
+++ db.js
@@ -2,8 +2,13 @@ const path = require('path');
 const { Sequelize, Model } = require('sequelize');
 
 // TODO - create the new sequelize connection
+const db = new Sequelize({
+    dialect: 'sqlite',
+    storage: path.join(__dirname, 'db.sqlite')
+});
 
+console.log(db);
 module.exports = {
-    sequelize,
+    db,
     Sequelize
 };
