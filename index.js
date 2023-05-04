6f761aa7bd69fab07c1d4c4301eb9030c9a9492a
Meher Tasnim <mehertasnim95@gmail.com>
2023-05-04 12:53:35 -0500

    added song-band association

--- index.js
+++ index.js
@@ -2,8 +2,11 @@ const { Band } = require('./models/Band')
 const { Musician } = require('./models/Musician')
 const { Song } = require("./models/Song")
 // Define associations here
+Band.hasMany(Song);
+Song.hasMany(Band);
 
-
+Band.belongsToMany(Song, { through: "band-song"});
+Song.belongsToMany(Band, { through: "band-song"});
 
 module.exports = {
     Band,
